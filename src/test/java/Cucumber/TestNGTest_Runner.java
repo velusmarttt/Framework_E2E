8141c7c89438c027376bc9181d17b9536760149d
velu smarttt <velusmiles3@gmail.com>
2024-08-16 22:52:45 +0530

    Cucumber report commit

--- src/test/java/Cucumber/TestNGTest_Runner.java
+++ src/test/java/Cucumber/TestNGTest_Runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/Cucumber", glue = "stepDefinitions",
-monochrome = true,tags = "@Regression", plugin = {"html:target/cucumber.html"})
+monochrome = true,tags = "@Regression", plugin = {"html:Cucumber_Report/cucumber.html"})
 public class TestNGTest_Runner extends AbstractTestNGCucumberTests {
 
 }
